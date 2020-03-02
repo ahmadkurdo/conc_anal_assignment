1352f7aa2a85a1936d446ce2532d35029b15f21b
Ahmed <ahmad.rizgar.rashid@gmail.com>
2020-03-02 13:02:13 +0100

    intial commit

---
+++ SocketClient/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+/* Note: If you are using .net core 2.1, you need to install System.Text.Json (use NuGet). */
+
+namespace SocketClient
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "IdentifierTypo")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class ClientInfo
+    {
+        public string studentnr { get; set; }
+        public string classname { get; set; }
+        public int clientid { get; set; }
+        public string teamname { get; set; }
+        public string ip { get; set; }
+        public string secret { get; set; }
+        public string status { get; set; }
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static class Message
+    {
+        public const string welcome = "WELCOME";
+        public const string stopCommunication = "COMC-STOP";
+        public const string statusEnd = "STAT-STOP";
+        public const string secret = "SECRET";
+    }
+
+    public class Client
+    {
+        private Socket _clientSocket;
+        private readonly ClientInfo _info;
+        private IPEndPoint _localEndPoint;
+        private readonly IPAddress _ipAddress = IPAddress.Parse("127.0.0.1");
+        private const int PortNumber = 11111;
+        private const int MinWaitingTime = 50, MaxWaitingTime = 100;
+        private readonly int _waitingTime;
+        private const string BaseStdNumber = "0700";
+
+        public Client(bool finishing, int n)
+        {
+            _waitingTime = new Random().Next(MinWaitingTime, MaxWaitingTime);
+            _info = new ClientInfo
+            {
+                classname = " DINF2 ",
+                studentnr = BaseStdNumber + n,
+                ip = "127.0.0.1",
+                clientid = finishing ? -1 : 1
+            };
+        }
+
+        private string GetClientInfo()
+        {
+            return JsonSerializer.Serialize(_info);
+        }
+
+        public void PrepareClient()
+        {
+            try
+            {
+                // Establish the remote endpoint for the socket.
+                _localEndPoint = new IPEndPoint(_ipAddress, PortNumber);
+                // Creation TCP/IP Socket using  
+                _clientSocket = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Client] Preparation failed: {0}", e.Message);
+            }
+        }
+
+        private string ProcessMessage(string msg)
+        {
+            Console.WriteLine("[Client] from Server -> {0}", msg);
+            var replyMsg = "";
+
+            try
+            {
+                switch (msg)
+                {
+                    case Message.welcome:
+                        replyMsg = GetClientInfo();
+                        break;
+                    default:
+                        var c = JsonSerializer.Deserialize<ClientInfo>(msg);
+                        if (c.status == Message.statusEnd)
+                            replyMsg = Message.stopCommunication;
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Client] processMessage {0}", e.Message);
+            }
+
+            return replyMsg;
+        }
+
+        public void StartCommunication()
+        {
+            Console.Out.WriteLine("[Client] **************");
+            Thread.Sleep(_waitingTime);
+            // Data buffer 
+            var messageReceived = new byte[1024];
+            var stop = false;
+
+            try
+            {
+                // Connect Socket to the remote endpoint 
+                _clientSocket.Connect(_localEndPoint);
+                // print connected EndPoint information  
+                Console.WriteLine("[Client] connected to -> {0} ", _clientSocket.RemoteEndPoint);
+
+                while (!stop)
+                {
+                    // Receive the message using the method Receive().
+                    var numBytes = _clientSocket.Receive(messageReceived);
+                    var rcvdMsg = Encoding.ASCII.GetString(messageReceived, 0, numBytes);
+                    var reply = ProcessMessage(rcvdMsg);
+                    SendReply(reply);
+                    if (reply.Equals(Message.stopCommunication))
+                        stop = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void SendReply(string msg)
+        {
+            // Create the message to send
+            Console.Out.WriteLine("[Client] Message to be sent: {0}", msg);
+            var messageSent = Encoding.ASCII.GetBytes(msg);
+            _clientSocket.Send(messageSent);
+        }
+
+        public void EndCommunication()
+        {
+            Console.Out.WriteLine("[Client] End of communication to -> {0} ", _clientSocket.RemoteEndPoint);
+            _clientSocket.Shutdown(SocketShutdown.Both);
+            _clientSocket.Close();
+        }
+    }
+
+    public class ClientsSimulator
+    {
+        private readonly int _numberOfClients;
+        private readonly Client[] _clients;
+        private const int WaitingTimeForStop = 2000;
+
+
+        public ClientsSimulator(int n)
+        {
+            _numberOfClients = n;
+            _clients = new Client[_numberOfClients];
+            for (var i = 0; i < _numberOfClients; i++)
+            {
+                _clients[i] = new Client(false, i);
+            }
+        }
+
+        public void ConcurrentSimulation()
+        {
+            Console.Out.WriteLine("\n[ClientSimulator] Concurrent simulator is going to start ...");
+
+            var threads = new ArrayList();
+            for (var i = 0; i < _numberOfClients; i++)
+            {
+                var client = _clients[i];
+                var clientNumber = i;
+
+                var thread = new Thread(() =>
+                {
+                    Console.Out.WriteLine("Setting up client number {0}", clientNumber);
+                    client.PrepareClient();
+                    client.StartCommunication();
+                    client.EndCommunication();
+                });
+                thread.Start();
+                threads.Add(thread);
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            Console.Out.WriteLine("\n[ClientSimulator] All clients finished with their communications ... ");
+
+            Thread.Sleep(WaitingTimeForStop);
+
+            var endClient = new Client(true, -1);
+            endClient.PrepareClient();
+            endClient.StartCommunication();
+            endClient.EndCommunication();
+        }
+    }
+
+    internal static class Program
+    {
+        // Main Method 
+        private static void Main()
+        {
+            Console.Clear();
+            const int numberOfClients = 249;
+            new ClientsSimulator(numberOfClients).ConcurrentSimulation();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,165 @@
+﻿/*** Fill these lines with your complete information.
+ * Note: Incomplete information may result in FAIL.
+ * Member 1: [First and Last name, first member]: Maarten de Goede
+ * Member 2: [First name and Last name, second member]: Corné Visser
+ * Std Number 1: [Student number of the first member] 0966770
+ * Std Number 2: [Student number of the second member] 0965144
+ * Class: [what is your class, example INF2C] DINF2
+ ***/
+
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+
+namespace SocketServer
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    [SuppressMessage("ReSharper", "IdentifierTypo")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")]
+    public class ClientInfo
+    {
+        public string studentnr { get; set; }
+        public string classname { get; set; }
+        public int clientid { get; set; }
+        public string teamname { get; set; }
+        public string ip { get; set; }
+        public string secret { get; set; }
+        public string status { get; set; }
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static class Message
+    {
+        public const string welcome = "WELCOME";
+        public const string stopCommunication = "COMC-STOP";
+        public const string statusEnd = "STAT-STOP";
+        public const string secret = "SECRET";
+    }
+
+    public class Server
+    {
+        private readonly Mutex _mutex = new Mutex();
+
+        private Socket _listener;
+        private IPEndPoint _localEndPoint;
+        private readonly IPAddress _ipAddress = IPAddress.Parse("127.0.0.1");
+        private const int PortNumber = 11111;
+
+        private readonly LinkedList<ClientInfo> _clients = new LinkedList<ClientInfo>();
+
+        private const int ListeningQueueSize = 500;
+
+        public void PrepareServer()
+        {
+            var threads = new List<Thread>();
+
+            try
+            {
+                Console.WriteLine("[Server] is ready to start ...");
+                // Establish the local endpoint
+                _localEndPoint = new IPEndPoint(_ipAddress, PortNumber);
+                _listener = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Console.Out.WriteLine("[Server] A socket is established ...");
+                // associate a network address to the Server Socket. All clients must know this address
+                _listener.Bind(_localEndPoint);
+                // This is a non-blocking listen with max number of pending requests
+                _listener.Listen(ListeningQueueSize);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ... ");
+
+                    var socket = _listener.Accept();
+                    var thread = new Thread(() => HandleClient(socket));
+                    thread.Start();
+                    threads.Add(thread);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine(e.Message);
+            }
+        }
+
+        private void HandleClient(Socket socket)
+        {
+            SendReply(socket, Message.welcome);
+
+            while (true)
+            {
+                var bytes = new byte[1024];
+
+                var numByte = socket.Receive(bytes);
+                var data = Encoding.ASCII.GetString(bytes, 0, numByte);
+                var replyMsg = ProcessMessage(data);
+
+                if (replyMsg.Equals(Message.stopCommunication))
+                    break;
+                SendReply(socket, replyMsg);
+            }
+        }
+
+        private string ProcessMessage(string message)
+        {
+            Console.WriteLine("[Server] received from the client -> {0} ", message);
+
+            try
+            {
+                if (message == Message.stopCommunication)
+                    return Message.stopCommunication;
+
+                var clientInfo = JsonSerializer.Deserialize<ClientInfo>(message);
+
+                _mutex.WaitOne();
+                _clients.AddLast(clientInfo);
+                _mutex.ReleaseMutex();
+
+                if (clientInfo.clientid == -1)
+                    PrintClients();
+
+                clientInfo.secret = clientInfo.studentnr + Message.secret;
+                clientInfo.status = Message.statusEnd;
+                return JsonSerializer.Serialize(clientInfo);
+            }
+            catch (Exception e)
+            {
+                Console.Out.WriteLine("[Server] processMessage {0}", e.Message);
+                return "";
+            }
+        }
+
+        private static void SendReply(Socket connection, string message)
+        {
+            connection.Send(Encoding.ASCII.GetBytes(message));
+        }
+
+        private void PrintClients()
+        {
+            const string delimiter = " , ";
+            Console.Out.WriteLine("[Server] This is the list of clients communicated");
+            foreach (var client in _clients)
+                Console.WriteLine(client.classname + delimiter + client.studentnr + delimiter + client.clientid);
+
+            Console.Out.WriteLine("[Server] Number of handled clients: {0}", _clients.Count);
+
+            _clients.Clear();
+        }
+    }
+
+    internal static class Program
+    {
+        // Main Method 
+        private static void Main()
+        {
+            Console.Clear();
+            new Server().PrepareServer();
+        }
+    }
+}
